d1b15217482e6467ac36e0a59f1aeea1cfb0c301
Patrick <pdf@80columns.com>
2024-05-28 01:04:45 -0400

    adding header and some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
+import { useState } from 'react';
+
+import { Button, Grid, Paper, Typography } from '@mui/material';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css'
 
-import { Button, Grid, TextField } from '@mui/material';
-
+/*
+ * Normally the application routes are defined in this file, but this app only has
+ * one route so for simplicity the root component is defined here
+ */
 function App() {
+    const [passphrase, setPassphrase] = useState('');
+
+    function generatePassphrase() {
+        setPassphrase('foobar');
+    }
+
     return (
         <Grid container
-              direction='row'
+              direction='column'
               justifyContent='center'
               alignItems='center'
-              height='90vh'
+              gap={4}
+              height='80vh'
         >
-            <TextField id='passphrase'
-                       label='Passphrase'
-                       variant='outlined'
-            />
+            <Typography variant='h3'>
+                Passphraser
+            </Typography>
+            <Typography variant='subtitle2'>
+                Generate secure and memorable passphrases
+            </Typography>
+            <Paper variant='elevation' square={false} style={{ width: '40%', height: 100, padding: 10 }}>
+                <Grid container
+                      direction='row'
+                      justifyContent='center'
+                      alignItems='center'
+                      height='100%'
+                >
+                    <Typography variant='h5'>
+                        {passphrase}
+                    </Typography>
+                </Grid>
+            </Paper>
             <Button id='generate-passphrase'
                     variant='contained'
+                    size='large'
+                    onClick={generatePassphrase}
             >
                 Generate
             </Button>
@@ -23,4 +56,4 @@ function App() {
     );
 }
 
-export default App
+export default App;
