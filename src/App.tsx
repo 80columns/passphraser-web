8ff3182bdc75714ab013d3846499cac4362d8c26
Patrick <pdf@80columns.com>
2024-06-04 00:39:16 -0400

    adding copy button, updating dynamic width

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
 import { Button, Grid, Paper, Typography } from '@mui/material';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import AddIcon from '@mui/icons-material/Add';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -16,42 +18,75 @@ function App() {
     const [passphrase, setPassphrase] = useState('');
 
     function generatePassphrase() {
-        setPassphrase('foobar');
+        setPassphrase('{QUARANTINENINETEENS7425intermarineninteens`bootleggedninteenss');
     }
 
     return (
         <Grid container
+              id='content'
               direction='column'
               justifyContent='center'
               alignItems='center'
               gap={4}
-              height='80vh'
+              marginTop={25}
         >
-            <Typography variant='h3'>
-                Passphraser
-            </Typography>
-            <Typography variant='subtitle2'>
-                Generate secure and memorable passphrases
-            </Typography>
-            <Paper variant='elevation' square={false} style={{ width: '40%', height: 100, padding: 10 }}>
+            <Grid container
+                  id='header'
+                  direction='column'
+                  justifyContent='center'
+                  alignItems='center'
+                  gap={0}
+            >
+                <Typography variant='h3'>
+                    Passphraser
+                </Typography>
+                <Typography variant='subtitle1'
+                            color='GrayText'
+                >
+                    Generate secure and memorable passphrases
+                </Typography>
+            </Grid>
+            <Paper variant='elevation'
+                   square={false}
+                   style={{ minHeight: 180, padding: 30 }}
+            >
                 <Grid container
-                      direction='row'
+                      id='passphrase-container'
+                      direction='column'
                       justifyContent='center'
                       alignItems='center'
                       height='100%'
+                      gap={6}
                 >
-                    <Typography variant='h5'>
+                    <Typography variant='h5' minHeight={40} textAlign='center' style={{ maxWidth: '75vw', overflowWrap: 'break-word' }}>
                         {passphrase}
                     </Typography>
+                    <Grid container
+                          id='passphrase-actions'
+                          direction='row'
+                          justifyContent='center'
+                          alignItems='center'
+                          gap={4}
+                    >
+                        <Button id='copy-passphrase'
+                                variant='contained'
+                                size='large'
+                                onClick={() => navigator.clipboard.writeText(passphrase)}
+                                startIcon={<ContentCopyIcon fontSize='small' />}
+                        >
+                            Copy
+                        </Button>
+                        <Button id='new-passphrase'
+                                variant='contained'
+                                size='large'
+                                onClick={generatePassphrase}
+                                startIcon={<AddIcon fontSize='small' />}
+                        >
+                            New
+                        </Button>
+                    </Grid>
                 </Grid>
             </Paper>
-            <Button id='generate-passphrase'
-                    variant='contained'
-                    size='large'
-                    onClick={generatePassphrase}
-            >
-                Generate
-            </Button>
         </Grid>
     );
 }
